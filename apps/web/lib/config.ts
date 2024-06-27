7570dd2dd128bdf2649457bb8a32272708a5529c
Eyad <e.kam12@yahoo.com>
2024-06-27 04:13:27 -0400

    removed google

--- apps/web/lib/config.ts
+++ apps/web/lib/config.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 
 const configSchema = z.object({
   appUrl: z.string(),
-  googleTrackingId: z.string(),
   supabase: z.object({
     url: z.string(),
     anonKey: z.string(),
@@ -14,7 +13,6 @@ export const config = configSchema.parse({
     process.env.NEXT_PUBLIC_ENV_TYPE === 'production'
       ? process.env.NEXT_PUBLIC_VERCEL_URL
       : 'http://localhost:3000',
-  googleTrackingId: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
   supabase: {
     url: process.env.NEXT_PUBLIC_SUPABASE_URL,
     anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
